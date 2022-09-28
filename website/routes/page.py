f911176b8d8688cd2387cf3e74f9d8b4bbb6e7d2
Bryan <bryanpadero@gmail.com>
2022-09-28 23:13:25 +0400

    updated property_page route updated property_page.html layout and design added url to property_page.html

--- website/routes/page.py
+++ website/routes/page.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, flash, render_template, redirect, url_for, request
 from website import db
-from ..models import *
+from ..models.property import Property
 
 page = Blueprint("page", __name__)
 
 
 @page.route("/property_page")
 def property_page():
-    return render_template("property_page.html")
+    property_id = request.args.get("property_id")
+    selected_property = Property.query.get(property_id)
+    return render_template("property_page.html", selected_property=selected_property)
